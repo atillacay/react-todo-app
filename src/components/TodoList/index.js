52039e6adc90c2a52931116777e060f2cce552e5
atillacay <atillacay@gmail.com>
2021-11-04 01:17:08 +0300

    css fixes

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { TodoContext } from '../../TodoContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-regular-svg-icons'
-import { faCheck, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faTrashAlt, faPen } from '@fortawesome/free-solid-svg-icons'
 import FilterTodo from '../FilterTodo'
 
 function TodoList() {
@@ -10,6 +10,8 @@ function TodoList() {
     const circleIcon = <FontAwesomeIcon icon={faCircle} style={{ color: "#fff" }} />
     const checkIcon = <FontAwesomeIcon icon={faCheck} style={{ color: "007bff" }} />
     const trashIcon = <FontAwesomeIcon icon={faTrashAlt} />
+    const editIcon = <FontAwesomeIcon icon={faPen} style={{ marginRight: "8px" }} />
+
 
     const [ todoData, setTodoData ] = useContext(TodoContext)
     const [ todos, setTodos ] = useState(todoData)
@@ -18,6 +20,10 @@ function TodoList() {
         setTodoData(
             todoData.filter((item) => item.id !== id)
         )
+    }
+    const editTodo = (id) => {
+        alert(id)
+        //TODO: Edit flow will be added possibly a modal to edit item
     }
     const toggleStatus = (id) => {
         const tempState = [ ...todoData ]
@@ -48,6 +54,7 @@ function TodoList() {
                             <span>{item.completed ? checkIcon : circleIcon}</span>
                             <span className="todo-text">{item.title}</span>
                         </li>
+                        <span onClick={() => { editTodo(item.id) }}>{editIcon}</span>
                         <span onClick={() => { delTodo(item.id) }}>{trashIcon}</span>
                     </div>
                 )}
